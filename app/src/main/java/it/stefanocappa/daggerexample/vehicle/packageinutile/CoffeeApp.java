06ea5003eaf0905c3f79afd12f77bc573cc20e23
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-04-12 01:04:49 +0200

    removed useless class

--- app/src/main/java/it/stefanocappa/daggerexample/vehicle/packageinutile/CoffeeApp.java
+++
@@ -1,8 +0,0 @@
-//package it.stefanocappa.daggerexample.officialgooglesample;
-//
-//public class CoffeeApp {
-//
-//    public static void main(String[] args) {
-//        new Vehicle().startGplEngine(25, 20);
-//    }
-//}
