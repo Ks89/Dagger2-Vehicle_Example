302eaed76fe1cd08b584cd070d47fdbc4fb19cdc
Stefano Cappa <stefano.cappa@mail.polimi.it>
2016-04-14 23:00:34 +0200

    added apache v2 license everywhere

--- app/src/main/java/it/stefanocappa/daggerexample/vehicle/sprungmass/engine/ElectricEngine.java
+++ app/src/main/java/it/stefanocappa/daggerexample/vehicle/sprungmass/engine/ElectricEngine.java
@@ -1,4 +1,17 @@
-package it.stefanocappa.daggerexample.vehicle.sprungmass.engine;//package it.stefanocappa.daggerexample.vehicle.sprungmass.engine;
+/*
+Copyright 2016 Stefano Cappa
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package it.stefanocappa.daggerexample.vehicle.sprungmass.engine;
 
 import android.util.Log;
 
@@ -7,7 +20,6 @@ import javax.inject.Inject;
 import hugo.weaving.DebugLog;
 import it.stefanocappa.daggerexample.vehicle.sprungmass.Electric;
 import it.stefanocappa.daggerexample.vehicle.sprungmass.tank.Tank;
-
 
 /**
  * Created by Ks89 on 17/03/16.
